e6f816072ffc877de876d83c818b1d1476fdfe74
DijanaH80 <dijanahalilovic@live.com>
2025-03-29 08:01:26 -0700

    Created my computer Object

---
+++ src/day03_branching_pullrequest/MyComputerObject.java
@@ -0,0 +1,12 @@
+package day03_branching_pullrequest;
+
+public class MyComputerObject {
+    public static void main(String[] args) {
+
+ MyComputer myComputer = new MyComputer("apple MacBook", 2000.0);
+ System.out.println(myComputer);
+
+
+
+    }
+}
