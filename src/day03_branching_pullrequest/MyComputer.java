5d51a9d3c301e91ce0c5d55d05d761d699e1ae01
DijanaH80 <dijanahalilovic@live.com>
2025-03-29 08:01:04 -0700

    Updated MyComputer class

--- src/day03_branching_pullrequest/MyComputer.java
+++ src/day03_branching_pullrequest/MyComputer.java
@@ -5,6 +5,16 @@ public class MyComputer {
  public String model;
  public double price;
 
+ public MyComputer(String model, double price) {
+  this.model = model;
+  this.price = price;
+ }
 
-
+ @Override
+ public String toString() {
+  return "MyComputer{" +
+          "model='" + model + '\'' +
+          ", price=" + price +
+          '}';
+ }
 }
